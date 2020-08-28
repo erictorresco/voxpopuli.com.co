2fdf75f3443696192aa517d35a5c5198db357185
erictorresco <erictorres.co@gmail.com>
2020-08-28 10:52:57 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function IndexPage() {
+  return (
+    <div className="flex bg-white h-screen justify-center items-center">
+      <div className="mx-8 flex flex-col justify-center">
+        <img src="logo.png" alt="Logo" />
+        <span className="text-center">voxpopuli corporación</span>
+      </div>
+    </div>
+  );
+}
